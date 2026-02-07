660447c4434b909589bfbb8675852fdcc42df2fe
skills-introduction-to-github <naman.m888@gmail.com>
2026-02-07 20:03:21 +0530

    connected to MONGODB

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"  
+}
+);
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";  
+
+// const app = express();  
+
+// ;( async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+//         console.log("Connected to MongoDB");
+//         app.on("error", (error) => {
+//             console.log(error);
+//             throw error;    
+//         });     
+        
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);   
+//         })
+
+//     } catch (error) {
+//         console.log(error); 
+//         throw error;    
+        
+//     }
+
+// })
